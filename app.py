8a616ab22a9cd5333d3d9a68e9f9cd55c3087f05
Polyák Nándor <polyak.nandor@as.loc>
2025-11-29 18:42:43 +0100

    Add /info végpont (új oldal) és az üdvözlő szöveg frissítése magyar nyelvűre

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello DevOps! This is my simple HTTP app."
+    return "Hello DevOpsz! Ez egy szimpla HTTP appppp, most már egy feature branch változtatással."
+
+@app.route("/info")
+def info():
+    return "Ez egy /info végpont. Feature branch részeként lett hozzáadva a DevOps részeként."
 
 if __name__ == "__main__":
-    # 0.0.0.0 + 8080, hogy a feladat követelményének megfeleljünk
     app.run(host="0.0.0.0", port=8080)
